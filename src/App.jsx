99bc20017cb604f49bac53a5c394c7050d34df4d
tamagochita <mevere66@gmail.com>
2022-04-13 12:18:25 -0500

    uso nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 const App = () => {
+  const [tarea, setTarea] = React.useState('');
+  const [tareas, setTareas]= React.useState([]);
+  const agregarTarea = e => {
+    e.preventDefault()
+
+    if (!tarea.trim()) {
+      console.log('elemento vacio');
+      return
+    }
+    console.log(tarea);
+    setTareas([...tareas, { id: nanoid(), NombreTarea: tarea }])
+    setTarea('')
+  }
   return (
     <div className='container mt-5'>
       <h1 className='text-center'>crud simple</h1>
       <h1>
         <div className="row">
           <div className="col-8">
-           <h4 className="text-center">Lista de tareas</h4>
-           <ul className="list-group">
-             <li className="list-group-item">
-               <span className="lead float-start">Nombre tarea</span>
+            <h4 className="text-center">Lista de tareas</h4>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <span className="lead float-start">Nombre tarea</span>
 
-               <button className="btn btn-danger btn-sm float-end mt-2 mx-2">Eliminar</button>
-               <button className="btn btn-warning btn-sm float-end mt-2">Editar</button>
-             </li>
-           </ul>
+                <button className="btn btn-danger btn-sm float-end mt-2 mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-end mt-2">Editar</button>
+              </li>
+            </ul>
           </div>
           <div className="col-4">
-           <h4 className="text-center">Formulario</h4>
+            <h4 className="text-center">Formulario</h4>
+            <form onSubmit={agregarTarea}>
+              <input
+                type="text"
+                className="form-control mb-2"
+                placeholder='Ingrese tarea'
+                onChange={e => setTarea(e.target.value)}
+                value={tarea}
+              />
+              <div className="d-grid gap-2">
+                <button
+                  className="btn btn-dark btn-block"
+                  type='submit'
+                >Agregar</button>
+              </div>
+
+            </form>
           </div>
         </div>
       </h1>
